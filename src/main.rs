681a8da320df37121176e7a37cefd796be486fba
Anton Pavlov <anton.pavlov@gmail.com>
2020-07-01 22:44:50 -0400

    Call external methods from main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod helloworld;
+mod leap;
+mod nthprime;
+mod raindrops;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Exercises:");
+    println!("Hello World: {}", helloworld::hello());
+    println!("Leap year: Is 2020 a leap year? - {}", leap::is_leap_year(2020));
+    println!("Nth prime number: 19th prime number is {}", nthprime::nth(19));
+    println!("Raindrops: 15 - {}", raindrops::raindrops(15));
 }
