957b7507f9fc247c6755074257be67e29ef587ce
Anton Pavlov <anton.pavlov@gmail.com>
2020-07-02 16:56:04 -0400

    Add unittests

--- src/raindrops.rs
+++ src/raindrops.rs
@@ -24,10 +24,10 @@
 /// # Examples
 ///
 /// ```rust
-/// assert_eq!("Pling", raindrops::raindrops(3));
-/// assert_eq!("PlingPlang", raindrops::raindrops(15));
-/// assert_eq!("52", raindrops::raindrops(52));
-/// assert_eq!("PlingPlangPlong", raindrops::raindrops(105));
+/// assert_eq!("Pling", raindrops(3));
+/// assert_eq!("PlingPlang", raindrops(15));
+/// assert_eq!("52", raindrops(52));
+/// assert_eq!("PlingPlangPlong", raindrops(105));
 ///
 /// ```
 pub fn raindrops(n: u32) -> String {
@@ -59,4 +59,121 @@ pub fn raindrops(n: u32) -> String {
         result = format!("{}", n);
     }
     result
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_1() {
+        assert_eq!("1", raindrops(1));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_3() {
+        assert_eq!("Pling", raindrops(3));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_5() {
+        assert_eq!("Plang", raindrops(5));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_7() {
+        assert_eq!("Plong", raindrops(7));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_6() {
+        assert_eq!("Pling", raindrops(6));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_8() {
+        assert_eq!("8", raindrops(8));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_9() {
+        assert_eq!("Pling", raindrops(9));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_10() {
+        assert_eq!("Plang", raindrops(10));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_14() {
+        assert_eq!("Plong", raindrops(14));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_15() {
+        assert_eq!("PlingPlang", raindrops(15));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_21() {
+        assert_eq!("PlingPlong", raindrops(21));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_25() {
+        assert_eq!("Plang", raindrops(25));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_27() {
+        assert_eq!("Pling", raindrops(27));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_35() {
+        assert_eq!("PlangPlong", raindrops(35));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_49() {
+        assert_eq!("Plong", raindrops(49));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_52() {
+        assert_eq!("52", raindrops(52));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_105() {
+        assert_eq!("PlingPlangPlong", raindrops(105));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_3125() {
+        assert_eq!("Plang", raindrops(3125));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_12121() {
+        assert_eq!("12121", raindrops(12_121));
+    }
 }
