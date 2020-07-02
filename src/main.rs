fa2644d548224870e5434924d2e21864ecd211b7
Anton Pavlov <anton.pavlov@gmail.com>
2020-07-02 16:56:44 -0400

    Update formatting

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,13 @@ mod raindrops;
 fn main() {
     println!("Exercises:");
     println!("Hello World: {}", helloworld::hello());
-    println!("Leap year: Is 2020 a leap year? - {}", leap::is_leap_year(2020));
-    println!("Nth prime number: 19th prime number is {}", nthprime::nth(19));
+    println!(
+        "Leap year: Is 2020 a leap year? - {}",
+        leap::is_leap_year(2020)
+    );
+    println!(
+        "Nth prime number: 19th prime number is {}",
+        nthprime::nth(19)
+    );
     println!("Raindrops: 15 - {}", raindrops::raindrops(15));
 }
